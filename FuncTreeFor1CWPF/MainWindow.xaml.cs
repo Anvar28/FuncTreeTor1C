09bef44c79910b192c7a164065e4a1f1405a4106
Anvar Khis <apxi2@yandex.ru>
2020-03-06 17:50:32 +0500

    Перевод на WPF

---
+++ FuncTreeFor1CWPF/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FuncTreeFor1CWPF
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MyApp myApp;
+
+        public MainWindow()
+        {
+            myApp = new MyApp();
+            InitializeComponent();
+            this.DataContext = myApp;
+            tbxPathToSrc.DataContext = myApp;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            myApp.PathToSrc = @"d:\projects\C-sharp\FuncTreeFor1C\Тестовая конфа\";
+        }
+
+        private void BtnSelectPath_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                myApp.PathToSrc = fbd.SelectedPath;
+            }
+        }
+    }
+}
---
+++ FuncTreeFor1CWPF/MyApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FuncTreeFor1CWPF
+{
+    public class MyApp : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _pathToSrc;
+        public string PathToSrc
+        {
+            get { return _pathToSrc; }
+            set
+            {
+                if (_pathToSrc != value)
+                {
+                    _pathToSrc = value;
+                    OnPropertyChanged("PathToSrc");
+                }
+            }
+        }
+
+        private 
+    }
+}
